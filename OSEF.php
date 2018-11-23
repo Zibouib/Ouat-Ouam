dd4d1e1f3836bd8a60d81e4a08f7ae2274a74ffa
Zibouib <44287625+Zibouib@users.noreply.github.com>
2018-11-23 09:26:55 +0100

    Update OSEF.php

--- OSEF.php
+++ OSEF.php
@@ -1,6 +1,7 @@
 <?php
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
 
+//esegdxgrsfesfezef
 if(isset($_POST['forminscription'])) {
    $pseudo = htmlspecialchars($_POST['pseudo']);
    $mail = htmlspecialchars($_POST['mail']);
